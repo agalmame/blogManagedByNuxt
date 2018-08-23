7a5812ba40630c902dfbbb8c12f95b8e5df0e5c5
yassine <agamlmameyassine@gmail.com>
2018-08-23 21:21:32 +0100

    initial comit

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <main class="main-content">
+      <nuxt/>
+    </main>
+  </div>
+</template>
+
+<style>
+*{
+  box-sizing: border-box
+}
+body {
+  font-family: 'Raleway', Courier, monospace,sans-serif
+}
+.main-content {
+  margin-top: 
+}
+</style>
---
+++ pages/about/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <section id="about-page">
+        <h1>Hello</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            Ratione totam ut aliquid itaque nisi fuga
+             nobis. Illo doloribus blanditiis, suscipit 
+             reiciendis repudiandae deleniti iste minima 
+             quia. Tempore reprehenderit consectetur voluptate.</p>
+    </section>
+</template>
